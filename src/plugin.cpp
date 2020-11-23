d23503e7d9a30b61d6bb79c94ca5a8c51e5a3197
Denis Chevalier <denis.chevalier@qonto.eu>
2020-11-23 23:11:48 +0100

    Module: Create ASR module

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -16,6 +16,7 @@ void init(Plugin* p) {
 		p->addModel(modelDiff);
 		p->addModel(modelNot);
 		p->addModel(modelAbs);
+		p->addModel(modelASR);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
